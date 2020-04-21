484e8df8244ba452385d3ed329275a6392c68446
lyricalzy <60869689+lyricalzy@users.noreply.github.com>
2020-04-21 11:39:56 +0900

    Add files via upload

---
+++ src/model/AchieveDTO.java
@@ -0,0 +1,50 @@
+package model;
+
+public class AchieveDTO {
+	private int id;
+	private String title;
+	private String name;
+	private String time;
+	private String content;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
---
+++ src/model/ExpoPageDTO.java
@@ -0,0 +1,99 @@
+package model;
+
+public class ExpoPageDTO {
+
+	ExpoDAO dao = new ExpoDAO();
+	int page;
+	int totalCount = dao.countRow();
+	int countList = 9;
+	int totalPage;
+	int countPage = 10;
+	int startPage;
+	int endPage;
+
+	public ExpoPageDTO() {
+		this.endPage = startPage + countPage - 1;
+		if (endPage > totalPage) {
+			this.endPage = totalPage;
+		}
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		if (page < 1) {
+			this.page = 1;
+		} else if (page > getTotalPage()) {
+			this.page = getTotalPage();
+		} else {
+			this.page = page;
+		}
+
+	}
+
+	public int getTotalCount() {
+
+		return totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public int getCountList() {
+		return countList;
+	}
+
+	public void setCountList(int countList) {
+		this.countList = countList;
+	}
+
+	public int getTotalPage() {
+		totalPage = totalCount / countList;
+
+		if (totalCount % countList > 0) {
+			totalPage++;
+		}
+
+		if (totalPage < page) {
+			page = totalPage;
+		}
+		return totalPage;
+	}
+
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public int getCountPage() {
+		return countPage;
+	}
+
+	public void setCountPage(int countPage) {
+		this.countPage = countPage;
+	}
+
+	public int getStartPage() {
+		startPage = ((page - 1) / countPage) * countPage + 1;
+		return startPage;
+	}
+
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+
+	public int getEndPage() {
+		this.endPage = getStartPage() + getCountPage() - 1;
+		if (endPage > getTotalPage()) {
+			this.endPage = getTotalPage();
+		}
+
+		return endPage;
+	}
+
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+}
